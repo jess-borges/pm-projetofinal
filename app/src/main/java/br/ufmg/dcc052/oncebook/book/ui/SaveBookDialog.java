b6e8ae0de9ec504f15e51bfef01c37051a5d004d
Jéssica Borges <jess.a.borges@gmail.com>
2016-06-15 20:42:32 -0300

    Initial commit

---
+++ app/src/main/java/br/ufmg/dcc052/oncebook/book/ui/SaveBookDialog.java
@@ -0,0 +1,67 @@
+package br.ufmg.dcc052.oncebook.book.ui;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import br.ufmg.dcc052.oncebook.R;
+import br.ufmg.dcc052.oncebook.Showable;
+import br.ufmg.dcc052.oncebook.book.Book;
+import br.ufmg.dcc052.oncebook.book.BookPresenter;
+
+/**
+ * Created by xavier on 6/13/16.
+ */
+public class SaveBookDialog implements Showable, DialogInterface.OnClickListener {
+
+  private BookPresenter presenter;
+
+  private Dialog dialog;
+  private EditText etBookName;
+  private EditText etBookDescription;
+
+  public SaveBookDialog(BookPresenter presenter, Context context) {
+    this.presenter = presenter;
+
+    View formBookView = LayoutInflater.from(context).inflate(R.layout.form_book, null, false);
+    etBookName = (EditText) formBookView.findViewById(R.id.etBookName);
+    etBookDescription = (EditText) formBookView.findViewById(R.id.etBookDescription);
+
+    Book book = presenter.getSelectedBook();
+
+    if (book != null) {
+      etBookName.setText(book.getName());
+      etBookDescription.setText(book.getDescription());
+    }
+
+    dialog = new AlertDialog.Builder(context)
+      .setTitle(book == null ? "Create book" : "Edit book")
+      .setView(formBookView)
+      .setPositiveButton(book == null ? "Create" : "Save", this)
+      .create();
+  }
+
+  @Override
+  public void show() {
+    dialog.show();
+  }
+
+  @Override
+  public void onClick(DialogInterface dialog, int which) {
+    Book book = presenter.getSelectedBook();
+
+    if(book == null) {
+      book = new Book(etBookName.getText().toString(), etBookDescription.getText().toString());
+    } else {
+      book.setName(etBookName.getText().toString());
+      book.setDescription(etBookDescription.getText().toString());
+    }
+
+    presenter.saveBook(book);
+    dialog.dismiss();
+  }
+}
